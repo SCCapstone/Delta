f23c5698143592bee297541a912b0608b4a3dc11
lxaw <lawhalen@email.sc.edu>
2022-09-12 13:12:56 -0400

    start redux

---
+++ delta_web/delta/frontend/src/components/data/DataAccel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class DataAccel extends Component {
+  render() {
+    return (
+      <div>DataAccel List</div>
+    )
+  }
+}
+
+export default DataAccel
---
+++ delta_web/delta/frontend/src/components/data/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  render() {
+    return (
+      <div>Add Acceleration Data Form</div>
+    )
+  }
+}
+
+export default Form

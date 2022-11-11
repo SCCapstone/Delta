dc8a68f9f29591712b2fc8e3f0210a4851edd81f
Carter Marlowe <marlowc@email.sc.edu>
2022-11-11 11:09:51 -0500

    Cleaning up graphs and making them look better

--- delta_web/delta/frontend/src/components/home/Dashboard.js
+++ delta_web/delta/frontend/src/components/home/Dashboard.js
@@ -21,9 +21,17 @@ export class Dashboard extends Component {
       datasets: [
         {
           label: user.username,
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgb(6, 132, 164)",
+          borderColor: "rgb(6, 132, 164)",
           data: [0, 10, 5, 2, 20, 30, 45],
+          lineTension: 0.5,
+        },
+        {
+          label: "Average",
+          backgroundColor: "rgb(51, 204, 242)",
+          borderColor: "rgb(51, 204, 242)",
+          data: [0, 4, 6, 7, 3, 16, 10],
+          lineTension: 0.5,
         },
       ],
     };
@@ -33,9 +41,17 @@ export class Dashboard extends Component {
       datasets: [
         {
           label: user.username,
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgb(6, 132, 164)",
+          borderColor: "rgb(6, 132, 164)",
           data: [0, 5, 13, 16, 20, 25, 15],
+          lineTension: 0.5,
+        },
+        {
+          label: "Average",
+          backgroundColor: "rgb(51, 204, 242)",
+          borderColor: "rgb(51, 204, 242)",
+          data: [0, 4, 6, 7, 3, 16, 10],
+          lineTension: 0.5,
         },
       ],
     };
@@ -45,39 +61,56 @@ export class Dashboard extends Component {
       datasets: [
         {
           label: user.username,
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgb(6, 132, 164)",
+          borderColor: "rgb(6, 132, 164)",
           data: [0, 5, 13, 16, 20, 25, 15],
-        }
-      ]
-    }
+          lineTension: 0.5,
+        },
+        {
+          label: "Average",
+          backgroundColor: "rgb(51, 204, 242)",
+          borderColor: "rgb(51, 204, 242)",
+          data: [0, 4, 6, 7, 3, 16, 10],
+          lineTension: 0.5,
+        },
+      ],
+    };
 
     return (
       <div>
         <h1>
           Welcome back <strong>{user.username}</strong>.
         </h1>
-        <h3>Today is a great day.</h3>
+        <h3>Let's get you caught up</h3>
+
         <div class="container">
-          <div class="row align-items-start">
-            <div class="col centered">
-              <h2>Today</h2>
-              <div classname="graph1">
-                <Line data={daily_graph}></Line>
+          <div class="pt-5">
+            <div class="row align-items-center">
+              <div class="col centered">
+                <center>
+                  <h2>Today</h2>
+                </center>
+                <div classname="graph1">
+                  <Line data={daily_graph}></Line>
+                </div>
+              </div>
+              <div class="col centered">
+                <center>
+                  <h2>This Week</h2>
+                </center>
+                <div classname="graph2">
+                  <Line data={weekly_graph}></Line>
+                </div>
+              </div>
+              <div class="col centered">
+                <center>
+                  <h2>This Month</h2>
+                </center>
+                <div classname="graph3">
+                  <Line data={monthly_graph}></Line>
+                </div>
               </div>
             </div>
-            <div class="col">
-              <h2>This Week</h2>
-              <div classname="graph2">
-                <Line data={weekly_graph}></Line>
-              </div>
-            </div>
-            <div class="col">
-              <h2>This Month</h2>
-              <div classname="graph3">
-              <Line data={monthly_graph}></Line>
-              </div>
-              </div>
           </div>
         </div>
       </div>

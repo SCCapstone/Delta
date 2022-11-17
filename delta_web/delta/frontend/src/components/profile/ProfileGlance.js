784a20acc963201d6b43e28d6ffbb4f834adf8c3
nchithan <chithannaveen@gmail.com>
2022-11-17 02:24:41 -0500

    added the import style #146

--- delta_web/delta/frontend/src/components/profile/ProfileGlance.js
+++ delta_web/delta/frontend/src/components/profile/ProfileGlance.js
@@ -3,6 +3,21 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from "prop-types";
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    flexDirection: row;
+`;
+
+const LeftSide = styled.div`
+    width: 25%;
+`;
+
+const RightSide = styled.div`
+    width: 75%;
+    
+`;
 
 export class ProfileGlance extends Component {
     static propTypes = {
@@ -21,16 +36,31 @@ export class ProfileGlance extends Component {
     render(){
         const {isAuthenticated, user} = this.props.auth;
         return(
-            <div>
+            <div className='container'>
                 <h1>
-                    Your Information
+                    <center>Your Information</center>
                 </h1>
+                <Container>
+                
+                <LeftSide>
+                <img
+                    src="/media/closeup_african_american_woman.jpg"
+                    className="d-block w-100"
+                    alt="Close up of smiling African American woman"
+                    width='400px'
+                    height='100px'
+                />
+                </LeftSide>
                 <div>
+                    <RightSide>
                     <h4>First Name: {user.first_name}</h4>
                     <h4>Last Name: {user.last_name}</h4>
                     <h4>Email: {user.email}</h4>
                     <h4>Username: {user.username}</h4>
+                    </RightSide>
                 </div>
+                
+                </Container>
                 <span>
                     <Link to="/profile/detailed">
                         click to see detailed

1f1f5a660936d4292fbedcb735ba4532a8ddbf50
nchithan <chithannaveen@gmail.com>
2022-11-17 10:53:59 -0500

    Fixed image sizing issue #148

--- delta_web/delta/frontend/src/components/profile/ProfileDetailed.js
+++ delta_web/delta/frontend/src/components/profile/ProfileDetailed.js
@@ -40,7 +40,7 @@ export class ProfileDetailed extends Component {
                         <div class="card mb-4 mb-xl-0">
                             <div class="card-header">Profile Picture</div>
                             <div class="card-body text-center">
-                                <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt=""/>
+                                <img class="img-account-profile rounded-circle mb-2" src="/media/closeup_african_american_woman.jpg" alt="" width='300' height='300'/>
                                 <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
                                 <button class="btn btn-primary" type="button">Upload new image</button>
                             </div>

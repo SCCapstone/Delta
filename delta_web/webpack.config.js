a106f3373a3475e061210ba9ba60b1dc1f61d75a
lxaw <LAWHALEN@email.sc.edu>
2023-01-08 09:29:44 -0500

    reviews added

--- delta_web/webpack.config.js
+++ delta_web/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,use:'css-loader'
+                test: /\.css$/,use:['style-loader','css-loader']
             }
         ]    
     }

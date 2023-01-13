db98a87f6274129685e39060692dc9cf376db418
lxaw <LAWHALEN@email.sc.edu>
2023-01-13 16:45:59 -0500

    added base notification / notification review class

--- delta_web/delta/social/models.py
+++ delta_web/delta/social/models.py
@@ -35,3 +35,27 @@ class Review(models.Model):
     
     def __str__(self):
         return self.title
+
+# basic notification class
+# used as parent for all other notifications
+class BaseNotification(models.Model):
+    text = models.CharField(max_length=300,null=False)
+    read = models.BooleanField(default=False)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+# notifications that relate to a review
+class NotificationReview(BaseNotification):
+    # sender of notification, ie in the case of a review being created, 
+    # the sender is the creator of the review and the recipient is the csv file
+    # owner
+    sender = models.ForeignKey(User,on_delete=models.CASCADE,null=True,related_name="sender_notification_post_set")
+    # here null is false as you always need a recipient for a notification
+    recipient = models.ForeignKey(User,on_delete=models.CASCADE,null=False,related_name="recipient_notification_post_set")
+
+    Review = models.ForeignKey(Review,on_delete=models.CASCADE,related_name="notification_post_set",null=False)
+
+    def __str__(self):
+        return "Notification for review {}".format(Review.title)

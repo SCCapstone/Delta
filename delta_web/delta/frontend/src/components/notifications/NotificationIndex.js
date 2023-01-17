1960c90cdb8db18f3193994bcca868a4cde54362
lxaw <LAWHALEN@email.sc.edu>
2023-01-17 12:33:46 -0500

    adding review page

--- delta_web/delta/frontend/src/components/notifications/NotificationIndex.js
+++ delta_web/delta/frontend/src/components/notifications/NotificationIndex.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from "react-redux"
+import axios from 'axios';
 
 const NotificationIndex = (props) =>{
+    // notifications
+    const [arrNotifications, setArrNotifications] = useState([]);
+
+  // get notifications
+  //
+  const getNotifications = () =>{
+    console.log('here')
+    axios.get('/api/notification_review/',{headers:{'Content-Type':'application/json','Authorization': `Token ${props.auth.token}`}})
+    .then((res)=>{
+      setArrNotifications(res.data)
+      console.log(res.data);
+    })
+  }
+    useEffect(()=>{
+        getNotifications()
+    },[]);
     return (
         <div className="container">
             <h1>Notifications</h1>
@@ -9,4 +26,8 @@ const NotificationIndex = (props) =>{
     )
 }
 
-export default NotificationIndex;
+const mapStateToProps = state =>({
+    auth:state.auth
+})
+
+export default connect(mapStateToProps,{})(NotificationIndex);

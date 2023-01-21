592cc36f0cb2df9f444926efbd6a03f03930a162
lxaw <LAWHALEN@email.sc.edu>
2023-01-21 15:44:25 -0500

    have to make your own state!

--- delta_web/delta/frontend/test/Review.test.js
+++ delta_web/delta/frontend/test/Review.test.js
@@ -1,28 +1,47 @@
-// /** @jest-environment jsdom */
-// // https://www.youtube.com/watch?v=ML5egqL3YFE
-// import React from 'react'
-// import '@testing-library/jest-dom'
-// import {render, screen} from "@testing-library/react"
-// import Review from "../src/components/csvFile/Review"
+/** @jest-environment jsdom */
+// https://www.youtube.com/watch?v=ML5egqL3YFE
+import React from 'react'
+import '@testing-library/jest-dom'
+import {render, screen} from "@testing-library/react"
+import Review from "../src/components/csvFile/Review"
 
-// import { Provider } from 'react-redux'
-// import store from '../src/store'
+import { Provider } from 'react-redux'
+import store from '../src/store'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-// test('Should Render Review',()=>{
-//     const reviewData = {
-//         id:1,
-//         rating:5,
-//         title:"TestTitle",
-//         author_username:"TestUser",
-//         formatted_date:"10/22/22",
-//         author:1
-//     }
-//     const auth = {
-//         user:{id:1}
-//     }
-//     render(<Provider store={store}><Review 
-//         reviewData = {reviewData} auth={auth}
-//         /></Provider>);
-//     const headerElement = screen.getAllByTestId('header-1');
-//     expect(headerElement).toBeInTheDocument();
-// })
+test('Should Render Review',()=>{
+    const reviewData = {
+        id:1,
+        rating:5,
+        title:"TestTitle",
+        author_username:"TestUser",
+        formatted_date:"10/22/22",
+        author:1
+    }
+    const initialAuthState = {
+        token: "TEST",
+        isAuthenticated:true,
+        isLoading:false,
+        user:{id:1}
+    }
+    const auth = function(state = initialAuthState,action){
+        return {...state}
+    }
+    const reducers = combineReducers({
+        auth
+    })
+    const initialState = {}
+    const middleware = [thunk]
+    const store = createStore(
+        reducers,
+        initialState,
+        composeWithDevTools(applyMiddleware(...middleware))
+    )
+    render(<Provider store={store}><Review 
+        reviewData = {reviewData} auth={auth}
+        /></Provider>);
+    const headerElement = screen.getAllByTestId('header-1');
+    expect(headerElement).toBeInTheDocument();
+})

0f24e589e33a2fa855976e04fd7dd549ac90fec9
nchithan <chithannaveen@gmail.com>
2023-01-22 23:03:26 -0500

    Adding more data #284

--- delta_web/delta/social/tests/test_setup.py
+++ delta_web/delta/social/tests/test_setup.py
@@ -40,7 +40,16 @@ class TestSetUp(APITestCase):
             'file':"words.txt",
             'text':"The Description",
             'active':"True",
-            'rating':"6"
+            'rating':"4"
+        }
+
+        review_data_InvalidTitle={
+            'title':" This is a sentence that is 101 characters long, it has to be exactly 101 characters to meet the requirement. 101 ",
+            'author':"nav",
+            'file':"words.txt",
+            'text':"The Description",
+            'active':"True",
+            'rating':"4"
         }
         return super().setUp()
 

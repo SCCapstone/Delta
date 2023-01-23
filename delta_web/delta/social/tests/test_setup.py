24747632afc65f6953e0dffad503009bf23b0713
nchithan <chithannaveen@gmail.com>
2023-01-22 20:59:19 -0500

    setting up test folder for social #284

---
+++ delta_web/delta/social/tests/test_setup.py
@@ -0,0 +1,12 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+class TestSetUp(APITestCase):
+    
+    def setUp(self):
+        
+        return super().setUp()
+
+    def tearDown():
+
+        return super().tearDown()

8757a245267d602358e9ad248ff770ae71571148
nchithan <chithannaveen@gmail.com>
2023-01-22 22:55:22 -0500

    Adding test data #284

--- delta_web/delta/social/tests/test_setup.py
+++ delta_web/delta/social/tests/test_setup.py
@@ -4,7 +4,44 @@ from django.urls import reverse
 class TestSetUp(APITestCase):
     
     def setUp(self):
-        
+        self.reviews_url = reverse('Reviews')
+        self.NotifReviews_url = reverse('NotificationReviews')
+
+        review_data_InvalidRatingLower={
+            'title':"Test",
+            'author':"nav",
+            'file':"words.txt",
+            'text':"The Description",
+            'active':"True",
+            'rating':"-1"
+        }
+
+        review_data_InvalidRatingUpper={
+            'title':"Test",
+            'author':"nav",
+            'file':"words.txt",
+            'text':"The Description",
+            'active':"True",
+            'rating':"6"
+        }
+
+        review_data_ValidRating={
+            'title':"Test",
+            'author':"nav",
+            'file':"words.txt",
+            'text':"The Description",
+            'active':"True",
+            'rating':"4"
+        }
+
+        review_data_BlankTitle={
+            'title':"",
+            'author':"nav",
+            'file':"words.txt",
+            'text':"The Description",
+            'active':"True",
+            'rating':"6"
+        }
         return super().setUp()
 
     def tearDown():

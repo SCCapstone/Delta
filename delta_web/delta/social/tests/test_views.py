c022b9dd94a623b9c544d9eebdad95b79acaa546
nchithan <chithannaveen@gmail.com>
2023-01-25 01:09:39 -0500

    Fixed more test cases #284

--- delta_web/delta/social/tests/test_views.py
+++ delta_web/delta/social/tests/test_views.py
@@ -9,4 +9,15 @@ class TestViews(TestSetUp):
     def test_make_review_invalid_rating_lower(self):
         res = self.client.post(self.reviews_url, self.review_data_InvalidRatingLower, format="json")
         self.assertEqual(res.status_code, 401)
-   
+
+    def test_make_review_invalid_rating_upper(self):
+        res = self.client.post(self.reviews_url, self.review_data_InvalidRatingUpper, format="json")
+        self.assertEqual(res.status_code, 401)
+
+    def test_make_review_valid_input(self):
+        res = self.client.post(self.reviews_url, self.review_data_ValidRating, format="json")
+        self.assertEqual(res.status_code, 401)
+
+    def test_make_review_invalid_title(self):
+        res = self.client.post(self.reviews_url, self.review_data_InvalidTitle, format="json")
+        self.assertEqual(res.status_code, 401)

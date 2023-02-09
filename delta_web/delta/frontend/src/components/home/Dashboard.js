4c5b189b9734fdee809259db4e1f45c3d0f862b6
Carter Marlowe <marlowc@email.sc.edu>
2023-02-09 16:48:41 -0500

    Working on Homepage

--- delta_web/delta/frontend/src/components/home/Dashboard.js
+++ delta_web/delta/frontend/src/components/home/Dashboard.js
@@ -12,6 +12,68 @@ export class Dashboard extends Component {
   };
 
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+    //Defines the values that will be held within the daily graph
+    const daily_graph = {
+      labels: [0, 2, 4, 6, 8, 10, 12], //X values of daily graph, hours in this case
+      datasets: [
+        {
+          label: user.username,
+          backgroundColor: "rgb(6, 132, 164)",
+          borderColor: "rgb(6, 132, 164)",
+          data: [0, 10, 5, 2, 20, 30, 45], //User data for daily graph
+          lineTension: 0.5,
+        },
+        {
+          label: "Average",
+          backgroundColor: "rgb(51, 204, 242)",
+          borderColor: "rgb(51, 204, 242)",
+          data: [0, 4, 6, 7, 3, 16, 10], // General data for daily graph
+          lineTension: 0.5,
+        },
+      ],
+    };
+    //Defines the details that will be in the weekly graph
+    const weekly_graph = {
+      labels: [1, 2, 3, 4, 5, 6, 7], //X values of the graph, days of the week in this case
+      datasets: [
+        {
+          label: user.username,
+          backgroundColor: "rgb(6, 132, 164)",
+          borderColor: "rgb(6, 132, 164)",
+          data: [0, 5, 13, 16, 20, 25, 15], // User Data for daily graph
+          lineTension: 0.5,
+        },
+        {
+          label: "Average",
+          backgroundColor: "rgb(100, 204, 242)",
+          borderColor: "rgb(51, 204, 242)",
+          data: [0, 4, 6, 7, 3, 16, 10], // General Data for weekly graph
+          lineTension: 0.5,
+        },
+      ],
+    };
+    // Monthly grpah values
+    const monthly_graph = {
+      labels: [1, 5, 10, 15, 20, 25, 30], //monthly graph x values
+      datasets: [
+        {
+          label: user.username,
+          backgroundColor: "rgb(6, 132, 164)",
+          borderColor: "rgb(6, 132, 164)",
+          data: [0, 5, 10, 9, 17, 28, 20], //User Data for monthly graph
+          lineTension: 0.5,
+        },
+        {
+          label: "Average",
+          backgroundColor: "rgb(51, 204, 242)",
+          borderColor: "rgb(51, 204, 242)",
+          data: [0, 4, 3, 9, 4, 15, 13], //General Data for monthly graph
+          lineTension: 0.5,
+        },
+      ],
+    };
+
     return (
       /*
       Breaks the screen into three separate columns

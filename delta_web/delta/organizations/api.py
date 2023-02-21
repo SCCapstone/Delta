dc5df89578a22e7533ca5bed439f8ad25d8a10f9
j-blake-s <blakeseekings25@gmail.com>
2023-02-21 17:35:38 -0500

    Private Org files only visible to members

--- delta_web/delta/organizations/api.py
+++ delta_web/delta/organizations/api.py
@@ -5,6 +5,7 @@ from knox.models import AuthToken
 from organizations.models import Organization
 from unicodedata import name
 from rest_framework.decorators import action
+from itertools import chain
 
 from data.serializers import SerializerCSVFile
 
@@ -34,8 +35,18 @@ class ViewsetOrganizations(viewsets.ModelViewSet):
     def data_posts(self,request,*args,**kwargs):
         instance = self.get_object()
 
-        csvFiles = instance.uploaded_files.all()
-        
+        user_in_org = False
+        if request.user in instance.following_users.all():
+            user_in_org = True       
+            
+        PublicCsvFiles = instance.uploaded_files.filter(is_public=True)
+        PublicOrgCsvFiles = instance.uploaded_files.filter(is_public_orgs=True)
+
+        if user_in_org:
+            csvFiles = list(chain(PublicOrgCsvFiles, PublicCsvFiles))
+        else:
+            csvFiles = PublicCsvFiles
+
         serializer = SerializerCSVFile(csvFiles,many=True)
 
         return Response(serializer.data)

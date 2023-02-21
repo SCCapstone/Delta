b2d7d1bbec9e94efc89f3fecdc2c0ed72a21335e
Carter Marlowe <marlowc@email.sc.edu>
2023-02-21 10:54:26 -0500

    Updating Component

--- delta_web/delta/frontend/src/components/home/HomepageNotificationMessage.js
+++ delta_web/delta/frontend/src/components/home/HomepageNotificationMessage.js
@@ -5,21 +5,10 @@ const HomepageNotificationMessage = (props) => {
     <div className="container">
       <div className="p-3 d-flex align-items-center bg-light border-bottom">
         <div className="col mr-3">
-          <div className="text-truncate">Organization Update</div>
-          <div className="small">
-            Jacob Johnson has joined your organization "Valafar Lab"
-          </div>
+          <div className="text-truncate">{props.title}</div>
+          <div className="small">{props.notificationMessage}</div>
         </div>
-        <div className="text-right text-muted pt-1">3d</div>
-      </div>
-      <div className="p-3 d-flex align-items-center bg-light">
-        <div className="col text-left mr-3">
-          <div className="text-truncate">Review on File</div>
-          <div classNme="small">
-            User test1234 has added a review to your file "upload544"
-          </div>
-        </div>
-        <div className="text-right text-muted pt-1">4d</div>
+        <div className="text-right text-muted pt-1">{props.date}</div>
       </div>
     </div>
   );

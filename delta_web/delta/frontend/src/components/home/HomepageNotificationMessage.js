8470697b1a0e34823e19b1201a8a5b5ac3c8da8c
Carter Marlowe <marlowc@email.sc.edu>
2023-02-21 10:39:30 -0500

    Homepage notification component changes

--- delta_web/delta/frontend/src/components/home/HomepageNotificationMessage.js
+++ delta_web/delta/frontend/src/components/home/HomepageNotificationMessage.js
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
 
 const HomepageNotificationMessage = (props) => {
-    
-        return(
-    <div class="p-3 d-flex align-items-center bg-light border-bottom">
-                <div class="col mr-3">
-                  <div class="text-truncate">Organization Update</div>
-                  <div class="small">
-                    Jacob Johnson has joined your organization "Valafar Lab"
-                  </div>
-                </div>
-                <div class="text-right text-muted pt-1">3d</div>
-              </div>
-              <div class="p-3 d-flex align-items-center bg-light">
-                <div class="col text-left mr-3">
-                  <div class="text-truncate">Review on File</div>
-                  <div class="small">
-                    User test1234 has added a review to your file "upload544"
-                  </div>
-                </div>
-                <div class="text-right text-muted pt-1">4d</div>
-              </div>)
-    }
+  return (
+    <div className="container">
+      <div className="p-3 d-flex align-items-center bg-light border-bottom">
+        <div className="col mr-3">
+          <div className="text-truncate">Organization Update</div>
+          <div className="small">
+            Jacob Johnson has joined your organization "Valafar Lab"
+          </div>
+        </div>
+        <div className="text-right text-muted pt-1">3d</div>
+      </div>
+      <div className="p-3 d-flex align-items-center bg-light">
+        <div className="col text-left mr-3">
+          <div className="text-truncate">Review on File</div>
+          <div classNme="small">
+            User test1234 has added a review to your file "upload544"
+          </div>
+        </div>
+        <div className="text-right text-muted pt-1">4d</div>
+      </div>
+    </div>
+  );
+};
 
-}
+export default HomepageNotificationMessage;

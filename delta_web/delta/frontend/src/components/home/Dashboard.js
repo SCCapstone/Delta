1d8b0ac2958ccbd3b12325b785c6db62385cbd9c
Carter Marlowe <marlowc@email.sc.edu>
2023-02-24 14:52:31 -0500

    fixing sizing on notifications

--- delta_web/delta/frontend/src/components/home/Dashboard.js
+++ delta_web/delta/frontend/src/components/home/Dashboard.js
@@ -17,31 +17,51 @@ export class Dashboard extends Component {
 
   render() {
     const { isAuthenticated, user } = this.props.auth;
-    const data = [1, 2, 3, 4, 5, 6, 7, 8];
+    const data = [
+      {
+        id: 1,
+        title: "New post in your organization",
+        description:
+          "A new post has been created in your organization, Valafar Lab, go to the organization page to see what's there! The information containined in these notifications is manually input for the sake of showing the notification block under use. Later, notifications will actually be generated and displayed by the app",
+        date: "2-24-23",
+      },
+      {
+        id: 2,
+        title: "A new update has come to the website",
+        description:
+          "There's been a new update that allows users to send each other messages now, find people in your organization to try this out with!",
+        date: "2-20-23",
+      },
+      {
+        id: 3,
+        title: "You have unread messages",
+        description:
+          "You have 15 unread messages, make sure you go and check these so that you don't fall behind",
+        date: "1-5-23",
+      },
+    ];
     return (
       <div className="container">
         <h1>
           Welcome back <strong>{user.username}</strong>.
         </h1>
         <h3>Here's what you've missed.</h3>
-        <div className="row">
-          <div className="box shadow-sm rounded bg-light mb-3 border border-gray">
-            <Swiper
-              navigation={true}
-              modules={[Navigation]}
-              className="mySwiper"
-            >
-              {data.map((data, index) => (
-                <SwiperSlide>
-                  <HomepageNotificationMessage
-                    notificationTitle={data}
-                    notificationMessage={data}
-                    date={data}
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+
+        <div
+          className="box shadow-sm rounded bg-light mb-3 border border-gray"
+          style={{ height: "40vh" }}
+        >
+          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            {data.map((data, index) => (
+              <SwiperSlide>
+                <HomepageNotificationMessage
+                  notificationTitle={data.title}
+                  notificationMessage={data.description}
+                  date={data.date}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     );

f57833e6a61f8ba08618c431192966343717e675
Carter Marlowe <marlowc@email.sc.edu>
2023-03-20 20:36:56 -0400

    Commented ReviewForm.js

--- delta_web/delta/frontend/src/components/csvFile/ReviewForm.js
+++ delta_web/delta/frontend/src/components/csvFile/ReviewForm.js
@@ -1,96 +1,132 @@
-import React from 'react'
-import Star from './Star';
+/**
+ * Delta Project
+ *
+ * Authors:
+ * Lexington Whalen (@lxaw)
+ * Carter Marlowe (@Cmarlowe123)
+ * Vince Kolb-Lugo (@vancevince)
+ * Blake Seekings (@j-blake-s)
+ * Naveen Chithan (@nchithan)
+ *
+ * ReviewForm.js
+ *
+ * Details the form used to create a new review
+ * Displays all of the information used for a review and gives
+ * an area for the user to input the information.
+ * Also contains a 5 star scale selection for the rating of the review.
+ */
+import React from "react";
+import Star from "./Star";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 import styles from "./cssFile.module.css";
-import { connect } from 'react-redux';
-import { addReview } from '../../actions/review';
+import { connect } from "react-redux";
+import { addReview } from "../../actions/review";
 
 const ReviewForm = (props) => {
-    const id = props.csvFileId;
+  const id = props.csvFileId;
 
-    var [reviewState, setReviewState] = useState({
-        'title':'',
-        'text':'',
-        'rating':5,
-        'file':`${id}`,
-        'author':`${props.auth.user.id}`
-    });
+  var [reviewState, setReviewState] = useState({
+    title: "",
+    text: "",
+    rating: 5,
+    file: `${id}`,
+    author: `${props.auth.user.id}`,
+  });
 
-    const onChange = (e) =>{
-        const newState = {...reviewState,[e.target.name]:e.target.value}
-        setReviewState(newState);
-    }
+  /* UTILITY: Updates the review when individual pieces of the review are changed.
+   * INPUTS: Takes in the current state of the review.
+   * OUTPUTS: Updated state of the review.
+   */
+  const onChange = (e) => {
+    const newState = { ...reviewState, [e.target.name]: e.target.value };
+    setReviewState(newState);
+  };
 
-    const onSubmit = (e) =>{
-        e.preventDefault();
-        props.addReview(reviewState);
-        setTimeout(()=>{
-            // refresh reviews
-            props.handleSubmit();
-        },200)
-    }
+  /* UTILITY: Generates and adds the review upon submission.
+   * INPUTS: Current state of the review.
+   * OUTPUTS: Created review.
+   */
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.addReview(reviewState);
+    setTimeout(() => {
+      // refresh reviews
+      props.handleSubmit();
+    }, 200);
+  };
 
-    const RATINGS = ["Poor","Fair","Good","Very good","Excellent"]
-    const [ratingIndex,setRatingIndex] = useState(4);
-    const activeStar = {
-        fill:'yellow'
-    }
-    const changeRatingIndex = (index) =>{
-        setRatingIndex(index);
-        const newState = {...reviewState,'rating':parseInt(index) + 1}
-        setReviewState(newState);
-    }
+  const RATINGS = ["Poor", "Fair", "Good", "Very good", "Excellent"];
+  const [ratingIndex, setRatingIndex] = useState(4);
+  const activeStar = {
+    fill: "yellow",
+  };
+
+  /* UTILITY: Changes the index of the rating, increasing it by one.
+   * INPUTS: Current rating index.
+   * OUTPUTS: New review state with increased rating index.
+   */
+  const changeRatingIndex = (index) => {
+    setRatingIndex(index);
+    const newState = { ...reviewState, rating: parseInt(index) + 1 };
+    setReviewState(newState);
+  };
 
   return (
-    <form onSubmit = {onSubmit}>
-        <div className = "form-group">
-            <div className="d-flex justify-content-between">
-                <div className={styles.stars}>
-                    {
-                        RATINGS.map((rating, index) => (
-                            <Star 
-                                index={index} 
-                                key={rating} 
-                                changeRatingIndex={changeRatingIndex}
-                                style={ ratingIndex >= index ? activeStar : {}}
-                            />
-                        ))
-                    }
-                </div>
-                <div>
-                    <p>
-                        Rating: {RATINGS[ratingIndex] ? RATINGS[ratingIndex] : 'No rating present yet.'}
-                    </p>
-                </div>
-            </div>
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <div className="d-flex justify-content-between">
+          <div className={styles.stars}>
+            {RATINGS.map((rating, index) => (
+              <Star
+                index={index}
+                key={rating}
+                changeRatingIndex={changeRatingIndex}
+                style={ratingIndex >= index ? activeStar : {}}
+              />
+            ))}
+          </div>
+          <div>
+            <p>
+              Rating:{" "}
+              {RATINGS[ratingIndex]
+                ? RATINGS[ratingIndex]
+                : "No rating present yet."}
+            </p>
+          </div>
         </div>
-        <div className = "form-group">
-            <label htmlFor = "title">Title</label>
-            <input className = "form-control" id = "title"
-            name="title"
-            onChange = {onChange}
-            />
-            <small id = "titleHelp">Add a descriptive title.</small>
-        </div>
-        <div className = "form-group">
-            <label htmlFor = "description">Description</label>
-            <textarea type="text" className = "form-control" id = "description"
-            onChange = {onChange}
-            name = "text"
-            />
-            <small id = "descriptionHelp">Add a description.</small>
-        </div>
-        <button type="submit" className="btn btn-outline-success">
-            Submit
-        </button>
+      </div>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          className="form-control"
+          id="title"
+          name="title"
+          onChange={onChange}
+        />
+        <small id="titleHelp">Add a descriptive title.</small>
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Description</label>
+        <textarea
+          type="text"
+          className="form-control"
+          id="description"
+          onChange={onChange}
+          name="text"
+        />
+        <small id="descriptionHelp">Add a description.</small>
+      </div>
+      <button type="submit" className="btn btn-outline-success">
+        Submit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-const mapStateToProps = state =>({
-    auth:state.auth
-})
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-export default connect(mapStateToProps,{addReview})(ReviewForm)
+export default connect(mapStateToProps, { addReview })(ReviewForm);

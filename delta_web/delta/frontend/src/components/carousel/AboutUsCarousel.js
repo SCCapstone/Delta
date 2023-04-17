aee61dadebb3b86bede425086324447ef3e7fe01
Vince <kolblugo@email.sc.edu>
2023-04-17 18:52:45 -0400

    built out AboutUsCarousel.js

--- delta_web/delta/frontend/src/components/carousel/AboutUsCarousel.js
+++ delta_web/delta/frontend/src/components/carousel/AboutUsCarousel.js
@@ -32,6 +32,96 @@ export class AboutUsCarousel extends Component {
             <img
               src="/media/Home-page-initial-signup.png"
               className="img-fluid"
+              alt="home page after initial registration"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Website-Home-Page.png"
+              className="img-fluid"
+              alt="home page blank"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Profile-Preupdate.png"
+              className="img-fluid"
+              alt="profiel page before updating information"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Profile-postupdate.png"
+              className="img-fluid"
+              alt="profile page after updating information"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Website-Upload.png"
+              className="img-fluid"
+              alt="upload page"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Website-Upload-File-uploaded.png"
+              className="img-fluid"
+              alt="upload page after uploading file"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Website-Download.png"
+              className="img-fluid"
+              alt="download page"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Website-Download-Queue.png"
+              className="img-fluid"
+              alt="download page, queue"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Website-Download-file-downloaded.png"
+              className="img-fluid"
+              alt="download page after downloading file"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Community-page.png"
+              className="img-fluid"
+              alt="community page"
+              height="400"
+              width="600"
+            />
+          </div>
+          <div className='carousel-item'>
+            <img
+              src="/media/Community-page-specific-organization.png"
+              className="img-fluid"
               alt="registration-page"
               height="400"
               width="600"
